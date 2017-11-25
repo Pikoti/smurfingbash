8d1c6a050e1fe119655c09d631e0987fe294cdce
lucie <lucie.vigreux@laposte.net>
2017-11-25 21:44:08 +0100

    smurfingcsv

--- test.sh
+++ test.sh
@@ -7,12 +7,8 @@ smurfs=(
         heftysmurf
 )
 
-title=(
-	smurfname:  
-)
 
 today=$(date +"%Y-%m-%d")
-
 last_week_today=$(date -d "$today - 7 days" +"%Y-%m-%d")
 echo $today
 echo $last_week_today
@@ -20,14 +16,11 @@ day=$last_week_today
 
 while [ $day != $today ]; do
     echo $i; 
+    printf "\n%s\n" $day >> smurf.csv
     for smurf in "${smurfs[@]}"
     do
-        printf "%s\n" $title ;
-    done
-     for smurf in "${smurfs[@]}"
-    do
-        printf "\t%s\n" $smurf+$day;
-    done
+        printf "%s,%20s \n" $smurf $smurf+$day >> smurf.csv
+    done 
     next_day=$(date -d "$day +1 days" +"%Y-%m-%d")
     day=$next_day
-done
+done 
